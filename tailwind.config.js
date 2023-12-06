a24c4160767773a3536a757710e6a78820394525
Roksolana Veres <Roxolanka1@ukr.net>
2023-12-06 13:31:58 +0200

    changed names of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,26 +13,24 @@ export default {
     extend: {
       colors: {
         // primary
-        limeGreen: "hsl(163, 72%, 41%)",
+        red: "hsl(348, 97%, 39%)",
         brightRed: "hsl(356, 69%, 56%)",
-        facebook: "hsl(208, 92%, 53%)",
-        twitter: "hsl(203, 89%, 53%)",
-        instagramFrom: "hsl(37, 97%, 70%)",
-        instagramTo: "hsl(329, 70%, 58%)",
-        youTube: "hsl(348, 97%, 39%)",
-        toggleFrom: "hsl(210, 78%, 56%)",
-        toggleTo: "hsl(146, 68%, 55%)",
+        lightOrange: "hsl(37, 97%, 70%)",
+        pink: "hsl(329, 70%, 58%)",
+        blue: "hsl(208, 92%, 53%)",
+        bluishGreen: "hsl(210, 78%, 56%)",
+        limeGreen: "hsl(163, 72%, 41%)",
+        lightGreen: "hsl(146, 68%, 55%)",
         // dark theme
-        veryDarkBlueBg: "hsl(230, 17%, 14%)", //bg
-        veryDarkBlueTop: "hsl(232, 19%, 15%)", //Top BG Pattern
+        veryDarkBlue: "hsl(230, 17%, 14%)", //bg
         darkDesaturatedBlue: "hsl(228, 28%, 20%)", // Card BG
         desaturatedBlue: "hsl(228, 34%, 66%)", // text
-        darkCardHover: "#333A56",
+        lighterBlue: "#333A56", // dark card hover
         // light theme
         veryPaleBlue: "hsl(225, 100%, 98%)", //Top BG Pattern light
         lightGrayishBlue: "hsl(227, 47%, 96%)", // Card BG light
         darkGrayishBlue: "hsl(228, 12%, 44%)", // text light
-        lightCardHover: "#E1E3F0",
+        grayishBlue: "#E1E3F0", // light card hover
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
